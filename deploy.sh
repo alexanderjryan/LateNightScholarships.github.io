e2099a9791322ac304f0b2eb2739341b5ee19f36
Adam Tyler <adamtyler07@gmail.com>
2022-06-29 22:22:52 +0000

    Enable gitpod

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:alexanderjryan/LateNightScholarships.github.io.git master
+git push -f https://github.com/alexanderjryan/LateNightScholarships.github.io.git master
 
 cd -
